295a154359a9c9e856a9ae464e6a82d3e0ba5e12
Andre <psnmeee@gmail.com>
2023-01-27 17:14:49 -0500

    added outlextokens file

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,37 @@
 import AbstractLexer from "./lexical_analysis/AbstractLexer";
 import Lexer from "./lexical_analysis/Lexer";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import TokenType from "./lexical_analysis/TokenType";
 import { tokenToString } from "./common/stringHelpers";
 
-const file = readFileSync("./src/examples/example-polynomial.src", "utf-8");
 
-const lexer: AbstractLexer = new Lexer(file);
+const files = ["example-polynomial", "example-bubblesort", "lexnegativegrading", "lexpositivegrading"];
 
-let token = lexer.nextToken();
-let currentLine = token.position;
-let outputFile = "";
 
-while (true) {
-    if (token.type !== TokenType.EOF) {
-        if (token.position > currentLine) {
+files.forEach((filename) => {
+
+    const file = readFileSync(`./src/examples/${filename}.src`, "utf-8");
+    const lexer: AbstractLexer = new Lexer(file);
+
+    let token = lexer.nextToken();
+    let currentLine = token.position;
+    let outputFile = "";
+
+    while (true) {
+        if (token.type !== TokenType.EOF) {
+            if (token.position > currentLine) {
+                outputFile += "\n";
+            }
+            outputFile += tokenToString(token);
+            currentLine = token.position;
+        } else {
             outputFile += "\n";
+            break;
         }
-        outputFile += tokenToString(token);
-        currentLine = token.position;
-    } else {
-        outputFile += "\n";
-        break;
+
+        token = lexer.nextToken();
     }
 
-    token = lexer.nextToken();
-}
+    writeFileSync(`./src/examples/${filename}.outlextokens`, outputFile);
 
-console.log(outputFile);
+})
