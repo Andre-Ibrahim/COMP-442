8bc955d41bcedd2b81c6a8b3c347bc4a239ca238
Andre <psnmeee@gmail.com>
2023-04-17 09:59:04 -0400

    added error message

--- CodeOutput/example-error2.src.m
+++ CodeOutput/example-error2.src.m
@@ -3,5 +3,19 @@
                hlt
                entry
                addi r14, r0, topaddr
+               sub r1, r1, r1
+%storing 1 into lit1
+               addi r1, r0, 1
+               sw lit1(r0), r1
+                addi r1, r0, 0
+               sub r1, r1, r1
+% assigning lit1 to main0x
+               lw r1, lit1(r0)
+               sw main0x(r0), r1
+               addi r1, r0, 0
                hlt
 
+               % space for variable main0x
+main0x         res 8
+               % space for variable lit1
+lit1           res 4
