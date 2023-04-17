5f41778c33a9aca313d79bb203e1070dfe4035fe
Andre <psnmeee@gmail.com>
2023-04-17 16:08:31 -0400

    updated error files

--- CodeOutput/example-error.src.m
+++ CodeOutput/example-error.src.m
@@ -447,128 +447,128 @@ endwhile1
                jr r11
 % end of function
 
-               % space for variable POLYNOMIAL0return
-POLYNOMIAL0retures 8
-               % space for variable lit1
-lit1           res 4
-               % space for variable build2new_function
-build2new_functres 0
-               % space for variable LINEAR0return
-LINEAR0return  res 8
-               % space for variable evaluate1result
-evaluate1resultres 8
-               % space for variable lit2
-lit2           res 8
-               % space for variable temp1
-temp1          res 8
-               % space for variable temp2
-temp2          res 8
-               % space for variable QUADRATIC0return
-QUADRATIC0returres 8
-               % space for variable evaluate1result
-evaluate1resultres 8
-               % space for variable temp3
-temp3          res 8
-               % space for variable temp4
-temp4          res 8
-               % space for variable temp5
-temp5          res 8
-               % space for variable temp6
-temp6          res 8
-               % space for variable lit3
-lit3           res 8
-               % space for variable lit4
-lit4           res 8
-               % space for variable build3new_function
-build3new_functres 0
-               % space for variable lit5
-lit5           res 4
-               % space for variable temp7
-temp7          res 8
-               % space for variable temp8
-temp8          res 8
-               % space for variable lit6
-lit6           res 4
-               % space for variable lit7
-lit7           res 4
-               % space for variable QUADRATIC0return
-QUADRATIC0returres 4
-               % space for variable f1i
-f1i            res 4
-               % space for variable f1i
-f1i            res 4
-               % space for variable f2i
-f2i            res 4
-               % space for variable f2j
-f2j            res 4
-               % space for variable f2return
-f2return       res 4
-               % space for variable f31p1
-f31p1          res 4
-               % space for variable f31return
-f31return      res 4
-               % space for variable main20a
-main20a        res 0
-               % space for variable main20c
-main20c        res 4
-               % space for variable main20f1
-main20f1       res 76
-               % space for variable main20f2
-main20f2       res 140
-               % space for variable main20counter
-main20counter  res 4
-               % space for variable main20counter
-main20counter  res 8
-               % space for variable main20counter
-main20counter  res 4
-               % space for variable main20counter
-main20counter  res 8
-               % space for variable main20i
-main20i        res 24
-               % space for variable main20j
-main20j        res 24
-               % space for variable lit8
-lit8           res 4
-               % space for variable lit9
-lit9           res 8
-               % space for variable lit10
-lit10          res 8
-               % space for variable lit11
-lit11          res 8
-               % space for variable lit12
-lit12          res 8
-               % space for variable lit13
-lit13          res 4
-               % space for variable lit14
-lit14          res 4
-               % space for variable lit15
-lit15          res 4
-               % space for variable lit16
-lit16          res 4
-               % space for variable lit17
-lit17          res 8
-               % space for variable lit18
-lit18          res 4
-               % space for variable lit19
-lit19          res 4
-               % space for variable lit20
-lit20          res 4
-               % space for variable temp9
-temp9          res 4
-               % space for variable lit21
-lit21          res 4
-               % space for variable lit22
-lit22          res 8
-               % space for variable temp10
-temp10         res 4
-               % space for variable temp11
-temp11         res 4
-               % space for variable lit23
-lit23          res 4
-               % space for variable lit24
-lit24          res 4
-               % space for variable temp12
-temp12         res 4
-               % space for variable buf
-buf            res 20
+                              % space for variable POLYNOMIAL0return
+POLYNOMIAL0return             res 8
+                              % space for variable lit1
+lit1                          res 4
+                              % space for variable build2new_function
+build2new_function            res 0
+                              % space for variable LINEAR0return
+LINEAR0return                 res 8
+                              % space for variable evaluate1result
+evaluate1result               res 8
+                              % space for variable lit2
+lit2                          res 8
+                              % space for variable temp1
+temp1                         res 8
+                              % space for variable temp2
+temp2                         res 8
+                              % space for variable QUADRATIC0return
+QUADRATIC0return              res 8
+                              % space for variable evaluate1result
+evaluate1result               res 8
+                              % space for variable temp3
+temp3                         res 8
+                              % space for variable temp4
+temp4                         res 8
+                              % space for variable temp5
+temp5                         res 8
+                              % space for variable temp6
+temp6                         res 8
+                              % space for variable lit3
+lit3                          res 8
+                              % space for variable lit4
+lit4                          res 8
+                              % space for variable build3new_function
+build3new_function            res 0
+                              % space for variable lit5
+lit5                          res 4
+                              % space for variable temp7
+temp7                         res 8
+                              % space for variable temp8
+temp8                         res 8
+                              % space for variable lit6
+lit6                          res 4
+                              % space for variable lit7
+lit7                          res 4
+                              % space for variable QUADRATIC0return
+QUADRATIC0return              res 4
+                              % space for variable f1i
+f1i                           res 4
+                              % space for variable f1i
+f1i                           res 4
+                              % space for variable f2i
+f2i                           res 4
+                              % space for variable f2j
+f2j                           res 4
+                              % space for variable f2return
+f2return                      res 4
+                              % space for variable f31p1
+f31p1                         res 4
+                              % space for variable f31return
+f31return                     res 4
+                              % space for variable main20a
+main20a                       res 0
+                              % space for variable main20c
+main20c                       res 4
+                              % space for variable main20f1
+main20f1                      res 76
+                              % space for variable main20f2
+main20f2                      res 140
+                              % space for variable main20counter
+main20counter                 res 4
+                              % space for variable main20counter
+main20counter                 res 8
+                              % space for variable main20counter
+main20counter                 res 4
+                              % space for variable main20counter
+main20counter                 res 8
+                              % space for variable main20i
+main20i                       res 24
+                              % space for variable main20j
+main20j                       res 24
+                              % space for variable lit8
+lit8                          res 4
+                              % space for variable lit9
+lit9                          res 8
+                              % space for variable lit10
+lit10                         res 8
+                              % space for variable lit11
+lit11                         res 8
+                              % space for variable lit12
+lit12                         res 8
+                              % space for variable lit13
+lit13                         res 4
+                              % space for variable lit14
+lit14                         res 4
+                              % space for variable lit15
+lit15                         res 4
+                              % space for variable lit16
+lit16                         res 4
+                              % space for variable lit17
+lit17                         res 8
+                              % space for variable lit18
+lit18                         res 4
+                              % space for variable lit19
+lit19                         res 4
+                              % space for variable lit20
+lit20                         res 4
+                              % space for variable temp9
+temp9                         res 4
+                              % space for variable lit21
+lit21                         res 4
+                              % space for variable lit22
+lit22                         res 8
+                              % space for variable temp10
+temp10                        res 4
+                              % space for variable temp11
+temp11                        res 4
+                              % space for variable lit23
+lit23                         res 4
+                              % space for variable lit24
+lit24                         res 4
+                              % space for variable temp12
+temp12                        res 4
+                              % space for variable buf
+buf                           res 20
 
